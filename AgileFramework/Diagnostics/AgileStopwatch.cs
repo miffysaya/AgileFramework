365beb492a14d8ad5c839be4d2c56e168dc9154d
Kagami <miffysaya@sina.cn>
2017-05-08 18:03:27 +0800

    20170508 18:03

---
+++ AgileFramework/Diagnostics/AgileStopwatch.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace AgileFramework.Diagnostics
+{
+    /// <summary>
+    /// 秒表
+    /// </summary>
+    public static class AgileStopwatch
+    {
+        /// <summary>
+        /// 运行
+        /// </summary>
+        /// <param name="action">行为</param>
+        /// <returns>结果</returns>
+        public static TimeSpan Execute(Action action)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            action();
+
+            stopwatch.Stop();
+            return stopwatch.Elapsed;
+        }
+    }
+}
---
+++ AgileFramework/Diagnostics/AgileTraceType.cs
@@ -0,0 +1,15 @@
+﻿namespace AgileFramework.Diagnostics
+{
+    public enum AgileTraceType
+    {
+        /// <summary>
+        /// 命令行
+        /// </summary>
+        Console = 1,
+
+        /// <summary>
+        /// 日志
+        /// </summary>
+        Log = 2
+    }
+}
